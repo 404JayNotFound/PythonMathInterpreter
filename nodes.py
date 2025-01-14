6fe327dde7df8b4e76440604bbe433075b510847
JAY♡ <jaymeoc04@gmail.com>
2025-01-14 00:06:22 +0000

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -2,22 +2,22 @@ class NumberNode:
     def __init__(self, value):
         self.value = value
 
-class BinaryOpNode:
-    def __init__(self, left, operator, right):
-        self.left = left
-        self.operator = operator
-        self.right = right
+class FunctionCallNode:
+    def __init__(self, name, arguments):
+        self.name = name
+        self.arguments = arguments
 
 class UnaryOpNode:
     def __init__(self, operator, operand):
         self.operator = operator
         self.operand = operand
 
+class BinaryOpNode:
+    def __init__(self, left, operator, right):
+        self.left = left
+        self.operator = operator
+        self.right = right
+
 class VariableNode:
     def __init__(self, name):
         self.name = name
-
-class FunctionCallNode:
-    def __init__(self, name, arguments):
-        self.name = name
-        self.arguments = arguments
